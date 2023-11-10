5c21790d2bcc1f72ee9af58e406d182b18f892ff
ImaGoodFella <steigerten@gmail.com>
2023-11-10 15:57:06 +0100

    added negative f1 score to callback loops

--- crop_prediction_framework/callbacks/evaluation.py
+++ crop_prediction_framework/callbacks/evaluation.py
@@ -8,9 +8,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.metrics import f1_score, balanced_accuracy_score, precision_score, recall_score, confusion_matrix
+from torchmetrics.classification import F1Score, Precision, Recall, Accuracy
+import torch.nn as nn
+
+args = {'task': 'binary'}
+        
+metrics = nn.ModuleDict({
+    "F1Score": F1Score(**args),
+    "Accuracy": Accuracy(**args),
+    "Precision": Precision(**args),
+    "Recall": Recall(**args),
+})
 
 def evaluate(model, data_loader, criterion, device, is_test=False, epoch=0, print_metrics=True):
     
+    metrics.to(device)
     model.eval()
     
     tot_loss = 0
@@ -43,6 +55,13 @@ def evaluate(model, data_loader, criterion, device, is_test=False, epoch=0, prin
         recall = recall_score(all_true, all_pred, average='macro')
 
         evaluation = 'Test' if is_test else 'Validation'
+
+        print(f'{evaluation} results for epoch {epoch}')
+        print('{:>8} | {:>8} | {:>9} | {:>6} | {:>5}'.format("f1-score", "accuracy", "precision", "recall", "loss"))
+        print('{:>8.4f} | {:>8.4f} | {:>9.4f} | {:>6.4f} | {:>5.4f}'.format(f1, accuracy, precision, recall, tot_loss.detach().cpu().numpy()))
+
+        res = [metric(torch.tensor(all_pred), torch.tensor(all_true)) for metric in metrics.values()]
+        f1, accuracy, precision, recall = res        
 
         print(f'{evaluation} results for epoch {epoch}')
         print('{:>8} | {:>8} | {:>9} | {:>6} | {:>5}'.format("f1-score", "accuracy", "precision", "recall", "loss"))
--- crop_prediction_framework/callbacks/training.py
+++ crop_prediction_framework/callbacks/training.py
@@ -2,10 +2,23 @@ import torch
 
 from tqdm import tqdm
 import numpy as np
-
+import torch.nn as nn
 from sklearn.metrics import f1_score, balanced_accuracy_score, precision_score, recall_score, confusion_matrix
+from torchmetrics.classification import F1Score, Precision, Recall, Accuracy
+
+
+args = {'task': 'binary'}
+        
+metrics = nn.ModuleDict({
+    "F1Score": F1Score(**args),
+    "Accuracy": Accuracy(**args),
+    "Precision": Precision(**args),
+    "Recall": Recall(**args),
+})
 
 def train_step(model, data_loader, criterion, optimizer, device, epoch=0, print_metrics=True):
+
+    metrics.to(device)
 
     model.train()
     
@@ -38,6 +51,13 @@ def train_step(model, data_loader, criterion, optimizer, device, epoch=0, print_
         accuracy = balanced_accuracy_score(all_true, all_pred)
         precision = precision_score(all_true, all_pred, average='macro')
         recall = recall_score(all_true, all_pred, average='macro')
+
+        print(f'Training results for epoch {epoch}')
+        print('{:>8} | {:>8} | {:>9} | {:>6} | {:>5}'.format("f1-score", "accuracy", "precision", "recall", "loss"))
+        print('{:>8.4f} | {:>8.4f} | {:>9.4f} | {:>6.4f} | {:>5.4f}'.format(f1, accuracy, precision, recall, tot_loss.detach().cpu().numpy()))
+
+        res = [metric(torch.tensor(all_pred), torch.tensor(all_true)) for metric in metrics.values()]
+        f1, accuracy, precision, recall = res        
 
         print(f'Training results for epoch {epoch}')
         print('{:>8} | {:>8} | {:>9} | {:>6} | {:>5}'.format("f1-score", "accuracy", "precision", "recall", "loss"))
@@ -82,5 +102,5 @@ def train_pure_step(model, data_loader, criterion, optimizer, device, epoch=0, p
         print(f'Training results for epoch {epoch}')
         print('{:>8} | {:>8} | {:>9} | {:>6} | {:>5}'.format("f1-score", "accuracy", "precision", "recall", "loss"))
         print('{:>8.4f} | {:>8.4f} | {:>9.4f} | {:>6.4f} | {:>5.4f}'.format(f1, accuracy, precision, recall, tot_loss.detach().cpu().numpy()))
-    
+
     return tot_loss
